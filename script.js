7cfaa77a1f232f171cf45418f2b77561e279ecc2
DuRyan Ebbesen <duryanebbesen@yahoo.com>
2020-12-13 03:49:22 -0800

    updated scripts.js)

--- script.js
+++ script.js
@@ -1,70 +1,114 @@
-const quizContainer = document.getElementById('quiz-container');
-const questionElement = document.getElementById('questions');
-const optionButtonsElement = document.getElementById('option-btns');
-const nextButton = document.getElementById('next-btn');
-const startQuiz = document.getElementById('start-btn');
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
+const myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  }
+];
 
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
+	function showQuestions(questions, quizContainer){
+    
+    // code will go here
+  
+	var output = [];
+	var answers;
 
-function Quiz() {
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
 
-questionContainerElement
-setNextQuestion()
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
 }
+	}
 
-function setNextQuestion() {
-showQuestion()
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add = ('btn')
-   
-    if (answer.correct) {
-        button.dataset.correct = answer.correct
+	function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+    // keep track of user's answers
+    var userAnswer = '';
+    var numCorrect = 0;
+    
+    for(var i=0; i<questions.length; i++){
+  
+      // find selected answer
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      
+      // if answer is correct
+      if(userAnswer===questions[i].correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+        
+     
+        answerContainers[i].style.color = 'lightgreen';
+      }
+     
+      else{
+       
+        answerContainers[i].style.color = 'red';
+      }
     }
-    button.addEventListener('click', selectAnswer)
- })
-}
+  
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+  }
+	
 
+	// show the questions
+  showQuestions(questions, quizContainer)
+  console.log(showQuestions);
 
-function selectAnswer(e) {
-
-    const questions = [
-        {
-            questions: "What Color is my Hair?",
-    
-            answers: [
-                {text: 'yellow', correct: false}
-                {text: 'blue', correct: false}
-                {text: 'black', correct: true}
-
-             ],
-            
-        }, 
-
-        {
-            questions: "What Color is my Hair?",
-    
-            answers: [
-                {text: 'red', correct: false}
-                {text: 'olive green', correct: true}
-                {text: 'black', correct: false}
-
-             ],
-            
-        }, 
-    
-    ];
-    
-}
-
-
-
-function resetState() {
-
-}
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
